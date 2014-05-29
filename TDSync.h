ca329d5ce4c2e609f8801f39473380654be44e57
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-05-29 14:59:31 -0500

    * rename TDLocking to TDSync

---
+++ TDSync.h
@@ -0,0 +1,16 @@
+//
+//  TDSync.h
+//  Panthro
+//
+//  Created by Todd Ditchendorf on 5/19/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TDSync <NSObject>
+
+- (void)acquire;
+- (void)relinquish;
+@end
+
