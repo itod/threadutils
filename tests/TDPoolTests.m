86ff4365db410bb8856e3cfded7c62168c47b317
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2015-05-22 10:54:03 -0500

    * pool test

--- tests/TDPoolTests.m
+++ tests/TDPoolTests.m
@@ -149,5 +149,51 @@
     }];
 }
 
+- (void)test2Items3Threads {
+    
+    id obj1 = @"one";
+    id obj2 = @"two";
+    self.pool = [TDPool poolWithItems:@[obj1, obj2]];
+    self.threshold = [TDThreshold thresholdWithValue:3];
+    
+    TDPerformOnBackgroundThreadAfterDelay(0.1, ^{
+        id obj = [pool takeItem];
+        TDEqualObjects(@"one", obj);
+        TDEquals(1, counter);
+        self.counter++;
+        sleep(2);
+        [pool returnItem:obj];
+        [self.threshold await];
+    });
+    
+    TDPerformOnBackgroundThreadAfterDelay(0.5, ^{
+        TDEquals(2, counter);
+        id obj = [pool takeItem];
+        TDEqualObjects(@"one", obj);
+        self.counter++;
+        [self.threshold await];
+        [pool returnItem:obj];
+    });
+    
+    id obj = [pool takeItem];
+    TDEqualObjects(@"two", obj);
+    TDEquals(0, counter);
+    self.counter++;
+    [self.threshold await];
+    [pool returnItem:obj];
+    
+    [done fulfill];
+    
+    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError *err) {
+        TDNil(err);
+        TDEquals(3, counter);
+        
+        id took1 = [pool takeItem];
+        TDEqualObjects(@"two", took1);
+        id took2 = [pool takeItem];
+        TDEqualObjects(@"one", took2);
+    }];
+}
+
 @synthesize pool=pool;
 @end
