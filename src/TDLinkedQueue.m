fc8ac5f5d3ef6b24d81be9ac64debab53016e1d3
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-10-21 20:49:05 -0500

    * single threaded working

--- src/TDLinkedQueue.m
+++ src/TDLinkedQueue.m
@@ -8,8 +8,38 @@
 
 #import "TDLinkedQueue.h"
 
+@interface LQNode : NSObject
+@property (retain) id object;
+@property (retain) LQNode *next;
+@end
+
+@implementation LQNode
+
+- (instancetype)init {
+    self = [self initWithObject:nil];
+    return self;
+}
+
+
+- (instancetype)initWithObject:(id)obj {
+    self = [super init];
+    if (self) {
+        self.object = obj;
+    }
+    return self;
+}
+
+
+- (void)dealloc {
+    self.object = nil;
+    [super dealloc];
+}
+
+@end
+
 @interface TDLinkedQueue ()
-@property (retain) id head;
+@property (retain) LQNode *head;
+@property (retain) LQNode *last;
 @end
 
 @implementation TDLinkedQueue
@@ -19,19 +49,43 @@
 }
 
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.head = [[[LQNode alloc] init] autorelease];
+        self.last = _head;
+    }
+    return self;
+}
+
+
 - (void)dealloc {
     self.head = nil;
+    self.last = nil;
     [super dealloc];
 }
 
 
 - (void)put:(id)obj {
+    NSParameterAssert(obj);
     
+    LQNode *node = [[[LQNode alloc] initWithObject:obj] autorelease];
+    
+    NSAssert(_last, @"");
+    _last.next = node;
+    self.last = node;
 }
 
 
 - (id)poll {
     id obj = nil;
+    
+    NSAssert(_head.next, @"");
+    LQNode *node = [[_head.next retain] autorelease];
+    _head.next = node.next;
+    
+    obj = [[node.object retain] autorelease];
+    node.object = nil;
     
     return obj;
 }
