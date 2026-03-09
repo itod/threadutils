3bf4b3b7d2bc20b3db022f8e00fb112116786b95
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2026-03-09 10:03:21 -0500

    * fix runnable.m location

--- src/TDRunnable.m
+++ src/TDRunnable.m
@@ -1,86 +1,54 @@
 //
-//  TDWorker.m
+//  TDRunnable.m
 //  TDThreadUtils
 //
 //  Created by Todd Ditchendorf on 4/14/25.
 //  Copyright © 2025 Todd Ditchendorf. All rights reserved.
 //
 
-#import <TDThreadUtils/TDRunner.h>
 #import <TDThreadUtils/TDRunnable.h>
-#import <TDThreadUtils/TDChannel.h>
 
-@interface TDRunner ()
-@property (nonatomic, retain) id <TDRunnable>runnable;
-@property (nonatomic, retain) id <TDChannel>inputChannel;
-@property (nonatomic, retain) id <TDChannel>outputChannel;
-@property (nonatomic, assign) NSUInteger number;
+@interface TDRunnable ()
+@property (nonatomic, assign, readwrite) id <TDRunnableDelegate>delegate;
 @end
 
-@implementation TDRunner
+@implementation TDRunnable
 
-+ (TDRunner *)runnerWithRunnable:(id <TDRunnable>)runnable inputChannel:(id <TDChannel>)ic outputChannel:(id <TDChannel>)oc number:(NSUInteger)i {
-    return [[[self alloc] initWithRunnable:runnable inputChannel:ic outputChannel:oc number:i] autorelease];
+- (instancetype)init {
+    NSAssert(0, @"");
+    self = [self initWithDelegate:nil];
+    return self;
 }
 
 
-- (instancetype)initWithRunnable:runnable inputChannel:(id <TDChannel>)ic outputChannel:(id <TDChannel>)oc number:(NSUInteger)i {
++ (TDPipelineStageType)pipelineStageType {
+    return TDPipelineStageTypeDeterminate;
+}
+
+
++ (BOOL)isBottleneck {
+    return NO;
+}
+
+
+- (instancetype)initWithDelegate:(id <TDRunnableDelegate>)d {
     self = [super init];
     if (self) {
-        self.runnable = runnable;
-        self.inputChannel = ic;
-        self.outputChannel = oc;
-        self.number = i;
+        self.delegate = d;
     }
     return self;
 }
 
 
 - (void)dealloc {
-    self.runnable = nil;
-    self.inputChannel = nil;
-    self.outputChannel = nil;
-    
-    self.titleText = nil;
-    self.infoText = nil;
+    self.delegate = nil;
     [super dealloc];
 }
 
 
-#pragma mark -
-#pragma mark Public
-
-- (void)run {
-    NSAssert(_inputChannel, @"");
-    
-    self.progress = 0.0;
-    
-    for (;;) {
-        id input = [_inputChannel take];
-        
-        NSAssert(_runnable, @"");
-        NSError *err = nil;
-        id output = [_runnable runWithInput:input error:&err];
-        
-        NSAssert(_outputChannel, @"");
-        [_outputChannel put:output];
-    }
-}
-
-
-#pragma mark -
-#pragma mark TDRunnableDelegate
-
-- (void)runnable:(id<TDRunnable>)r updateProgress:(double)d {
-    NSAssert(_runnable == r, @"");
-    self.progress = d;
-}
-
-
-- (void)runnable:(id<TDRunnable>)r updateTitleText:(NSString *)title infoText:(NSString *)info {
-    NSAssert(_runnable == r, @"");
-    self.titleText = title;
-    self.infoText = info;
+- (id)runWithInput:(id)input error:(NSError **)outErr {
+    NSAssert2(0, @"%s is an abstract method and must be implemented in %@", __PRETTY_FUNCTION__, [self class]);
+    return nil;
 }
 
 @end
