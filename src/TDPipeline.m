8f174ac7cf4ed3d1fc2bbd38e72c5830b435999a
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2026-08-15 10:31:15 -0500

    * cu

--- src/TDPipeline.m
+++ src/TDPipeline.m
@@ -154,7 +154,7 @@
 #pragma mark Private
 
 - (id <TDChannel>)newBoundedBuffer {
-    return [[TDBoundedBuffer alloc] initWithSize:5]; // TODO how to configure this?
+    return [[TDBoundedBuffer alloc] initWithSize:3]; // TODO how to configure this?
 }
 
 
